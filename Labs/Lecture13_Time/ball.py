397fd10007d5f88f1d69c68617e645f2028e0ba6
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-11-13 18:23:03 +0900

    Revert "Revert "Today_Study""
    This reverts commit 7756ba28f73116b685a8ee9cba0be5988c4800fb.

--- Labs/Lecture13_Time/ball.py
+++ Labs/Lecture13_Time/ball.py
@@ -1,13 +1,16 @@
 from pico2d import *
 import game_world
 
+BALL_SPEED = 15
+
 class Ball:
     image = None
 
     def __init__(self, x = 400, y = 300, velocity = 1):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y, self.velocity = x, y, velocity
+
+        self.x, self.y, self.velocity = x, y, velocity * BALL_SPEED
 
     def draw(self):
         self.image.draw(self.x, self.y)
