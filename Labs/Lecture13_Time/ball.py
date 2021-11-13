7756ba28f73116b685a8ee9cba0be5988c4800fb
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-11-13 18:13:42 +0900

    Revert "Today_Study"
    This reverts commit 5e84b25c7dce21fae8de22ca7cc3db7851fdeee2.

--- Labs/Lecture13_Time/ball.py
+++ Labs/Lecture13_Time/ball.py
@@ -1,16 +1,13 @@
 from pico2d import *
 import game_world
 
-BALL_SPEED = 15
-
 class Ball:
     image = None
 
     def __init__(self, x = 400, y = 300, velocity = 1):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-
-        self.x, self.y, self.velocity = x, y, velocity * BALL_SPEED
+        self.x, self.y, self.velocity = x, y, velocity
 
     def draw(self):
         self.image.draw(self.x, self.y)
--- Labs/Lecture13_Time/boy.py
+++ Labs/Lecture13_Time/boy.py
@@ -6,17 +6,17 @@ import game_world
 
 # Boy Run Speed
 # fill expressions correctly
-PIXEL_PER_METER = (10.0 / 0.3) # 10 픽셀당 30 cm
-RUN_SPEED_KMPH = 20.0 # Km/h
-RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
-RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
-RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+PIXEL_PER_METER = 0
+RUN_SPEED_KMPH = 0
+RUN_SPEED_MPM = 0
+RUN_SPEED_MPS = 0
+RUN_SPEED_PPS = 0
 
 # Boy Action Speed
 # fill expressions correctly
-TIME_PER_ACTION = 0.5
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
+TIME_PER_ACTION = 0
+ACTION_PER_TIME = 0
+FRAMES_PER_ACTION = 0
 
 
 
@@ -68,24 +68,16 @@ class IdleState:
 class RunState:
 
     def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += RUN_SPEED_PPS
-        elif event == LEFT_DOWN:
-            boy.velocity -= RUN_SPEED_PPS
-        elif event == RIGHT_UP:
-            boy.velocity -= RUN_SPEED_PPS
-        elif event == LEFT_UP:
-            boy.velocity += RUN_SPEED_PPS
-
-        boy.dir = clamp(-1, boy.velocity, 1)
+        # fill here
+        pass
 
     def exit(boy, event):
         if event == SPACE:
             boy.fire_ball()
 
     def do(boy):
-        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        boy.x += boy.velocity * game_framework.frame_time
+        # fill here
+        boy.frame = (boy.frame + 1) % 8
         boy.x = clamp(25, boy.x, 1600 - 25)
 
     def draw(boy):
@@ -129,7 +121,7 @@ class Boy:
         self.x, self.y = 1600 // 2, 90
         # Boy is only once created, so instance image loading is fine
         self.image = load_image('animation_sheet.png')
-        self.font = load_font('ENCR10B.TTF', 16)
+        # fill here
         self.dir = 1
         self.velocity = 0
         self.frame = 0
@@ -139,7 +131,7 @@ class Boy:
 
 
     def fire_ball(self):
-        ball = Ball(self.x, self.y, self.dir)
+        ball = Ball(self.x, self.y, self.dir*3)
         game_world.add_object(ball, 1)
 
 
@@ -156,7 +148,7 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % get_time(), (255, 255, 0))
+        # fill here
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
