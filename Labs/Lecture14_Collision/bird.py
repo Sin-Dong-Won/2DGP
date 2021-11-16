3cb5e25d9f8d1f4f7513c0e11d3fd55eb88951a2
LeeHyRy <ukrk774@kpu.ac.kr>
2021-11-16 18:04:34 +0900

    새 날아다니기 수정

--- Labs/Lecture14_Collision/bird.py
+++ Labs/Lecture14_Collision/bird.py
@@ -4,17 +4,17 @@ import random
 
 import game_world
 
-# Boy Run Speed
+# Bird Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
 BIRD_SPEED_KMPH = 1.0  # Km / Hour
 BIRD_SPEED_MPM = (BIRD_SPEED_KMPH * 1000.0 / 60.0)
 BIRD_SPEED_MPS = (BIRD_SPEED_MPM / 60.0)
 BIRD_SPEED_PPS = (BIRD_SPEED_MPS * PIXEL_PER_METER)
 
-# Boy Action Speed
-TIME_PER_ACTION = 0.5
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
+# Bird Action Speed
+TIME_PER_ACTION = 2
+ACTION_PER_TIME = 10.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 1
 
 class Bird:
     def __init__(self):
