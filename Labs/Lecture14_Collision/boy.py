b07f38a4a2e3a82e85973b643217f63790b56bb0
LeeHyRy <ukrk774@kpu.ac.kr>
2021-11-16 19:06:40 +0900

    Today Study

--- Labs/Lecture14_Collision/boy.py
+++ Labs/Lecture14_Collision/boy.py
@@ -137,7 +137,7 @@ class Boy:
 
     def get_bb(self):
         # fill here
-        return 0, 0, 0, 0
+        return self.x - 50, self.y - 50, self.x + 50, self.y + 50
 
 
     def fire_ball(self):
@@ -159,7 +159,7 @@ class Boy:
     def draw(self):
         self.cur_state.draw(self)
         self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % get_time(), (255, 255, 0))
-        #fill here
+        draw_rectangle(*self.get_bb())
 
 
     def handle_event(self, event):
--- Labs/Lecture14_Collision/main_state.py
+++ Labs/Lecture14_Collision/main_state.py
@@ -8,7 +8,7 @@ import game_world
 
 from boy import Boy
 from grass import Grass
-from ball import Ball
+from ball import Ball, BigBall
 from bird import Bird
 
 name = "MainState"
@@ -22,10 +22,19 @@ big_balls = []
 
 
 def collide(a, b):
-    # fill here
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b:
+        return False
+    if right_a < left_b:
+        return False
+    if top_a < bottom_b:
+        return False
+    if bottom_a > top_b:
+        return False
+
     return True
-
-
 
 
 def enter():
@@ -44,11 +53,14 @@ def enter():
         bird1 = Bird()
         game_world.add_object(bird1, 1)
 
-    # fill here for balls
+    global balls
+    balls = [Ball() for i in range(10)] + [BigBall() for i in range(10)]
+    game_world.add_objects(balls, 1)
 
 
 def exit():
     game_world.clear()
+
 
 def pause():
     pass
@@ -64,7 +76,7 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-                game_framework.quit()
+            game_framework.quit()
         else:
             boy.handle_event(event)
 
@@ -73,8 +85,15 @@ def update():
     for game_object in game_world.all_objects():
         game_object.update()
 
-    # fill here for collision check
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            game_world.remove_object(ball)
+        for i in balls:
+            collide(grass, ball)
+            ball.stop()
 
+    # delay(0.9)
 
 
 def draw():
@@ -82,9 +101,3 @@ def draw():
     for game_object in game_world.all_objects():
         game_object.draw()
     update_canvas()
-
-
-
-
-
-
